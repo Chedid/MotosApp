c11b5ab63cf7a37ecf9789fd574bcf94b37223b7
Yago Chedid <yagorogerio020300@gmail.com>
2020-11-17 11:40:18 -0300

    Primeiro commit

---
+++ Componentes/Motos/styles.js
@@ -0,0 +1,17 @@
+
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 50,
+    fontWeight: "bold",
+    margin: "10%",
+    color: "red"
+}
+})
+
+
+
